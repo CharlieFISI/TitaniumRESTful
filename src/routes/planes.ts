15698580359e19843c32f73f9f022d981687144d
LinuxChar <charlie.elizarbe@unmsm.edu.pe>
2023-02-28 02:49:11 -0500

    Ruta de Planes

--- src/routes/planes.ts
+++ src/routes/planes.ts
@@ -13,32 +13,4 @@ router.route('/:id')
   .delete(deleteIdEntry)
   .put(updateIdEntry)
 
-/*
-router.get('/:id', (req, res) => {
-  const plan = planesServicio.findById(+req.params.id)
-
-  return (plan != null)
-    ? res.send(plan)
-    : res.sendStatus(404)
-})
-
-router.post('/', (req, res) => {
-  try {
-    const newPlanEntry = toNewPlanEntry(req.body)
-    const addedPlanEntry = planesServicio.addPlan(newPlanEntry)
-
-    res.json(addedPlanEntry)
-  } catch (e: unknown) {
-    let message: string
-    if (e instanceof Error) {
-      message = e.message
-    } else {
-      message = String(e)
-    }
-
-    res.status(400).send(message)
-  }
-})
-*/
-
 export default router
