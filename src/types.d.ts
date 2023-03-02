6a2e76c31f202ad38cd0349749f74afcb2db6737
LinuxChar <charlie.elizarbe@unmsm.edu.pe>
2023-03-02 00:29:06 -0500

    Archivo de Tipado

--- src/types.d.ts
+++ src/types.d.ts
@@ -29,14 +29,36 @@ export interface ClaseEntry {
 
 export interface IngresoEntry {
   IngresoId: number
-  TipoIngreso: number
+  TipoIngresoId: number
   UsuarioId: number
   ClienteId: number
   MontoTotal: number
   Fecha: Date
 }
 
+export interface EntrenadorEntry {
+  EntrenadorId: number
+  Nombre: string
+  Apellido: string
+  Email: string
+  DNI: string
+  Telefono: number
+}
+
+export interface UsuarioEntry {
+  UsuarioId: number
+  Nombre: string
+  Apellido: string
+  DNI: string
+  Email: string
+  Telefono: number
+  Contrasenia: string
+  Creado: Date
+}
+
 export type PlanEntryWithoutId = Omit<PlanEntry, 'PlanId'>
 export type ClienteEntryWithoutId = Omit<ClienteEntry, 'ClienteId'>
 export type ClaseEntryWithoutId = Omit<ClaseEntry, 'ClaseId'>
-export type IngresoEntryWithoutId = Omit<IngresoEntry, 'ClaseId'>
+export type IngresoEntryWithoutId = Omit<IngresoEntry, 'IngresoId'>
+export type EntrenadorEntryWithoutId = Omit<EntrenadorEntry, 'EntrenadorId'>
+export type UsuarioEntryWithoutIdAndDate = Omit<UsuarioEntry, 'UsuarioId', 'Creado'>
