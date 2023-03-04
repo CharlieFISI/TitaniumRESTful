72a06084f5deea524d5920da900263ee38314c0d
LinuxChar <charlie.elizarbe@unmsm.edu.pe>
2023-03-04 02:51:12 -0500

    Aplicacion

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express, { Application } from 'express'
 
 import indexRouter from './index'
-import planRouter from './routes/planes'
-import clienteRouter from './routes/clientes'
 import claseRouter from './routes/clases'
+import clienteRouter from './routes/clientes'
 import entrenadorRouter from './routes/entrenadores'
+import fechaclaseRouter from './routes/fechaclases'
+import ingresoRouter from './routes/ingresos'
+import planRouter from './routes/planes'
+import planingresoRouter from './routes/planesingresos'
+import tipoingresoRouter from './routes/tipoingresos'
 import usuarioRouter from './routes/usuarios'
 
 export class App {
@@ -27,10 +31,14 @@ export class App {
 
   routes (): void {
     this.app.use(indexRouter)
-    this.app.use('/api/planes', planRouter)
-    this.app.use('/api/clientes', clienteRouter)
     this.app.use('/api/clases', claseRouter)
+    this.app.use('/api/clientes', clienteRouter)
     this.app.use('/api/entrenadores', entrenadorRouter)
+    this.app.use('/api/fechaclases', fechaclaseRouter)
+    this.app.use('/api/ingresos', ingresoRouter)
+    this.app.use('/api/planes', planRouter)
+    this.app.use('/api/planingresos', planingresoRouter)
+    this.app.use('/api/tipoingresos', tipoingresoRouter)
     this.app.use('/api/usuarios', usuarioRouter)
   }
 
